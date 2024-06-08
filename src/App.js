091da84419984710896adace7537b9d1e903ec06
Rishijay Shrivastava <rishijay.shrivastava@gmail.com>
2024-06-08 18:51:25 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar color="dark" light expand="md">
+          <NavbarBrand href="/" className="Header-Text">
+            Header-Router React App
+          </NavbarBrand>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink tag={Link} to="/" className="Header-Text">
+                Home
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} to="/about" className="Header-Text">
+                About
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} to="/contact" className="Header-Text">
+                Contact
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar>
+        <div className="container mt-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+// src/pages/About.js
+import React from "react";
+
+function About() {
+  return (
+    <iframe
+      src="https://react.dev/"
+      title="uniqueTitle"
+      style={{ width: "100%", height: "100vh" }}
+    />
+  );
+}
+
+export default About;
