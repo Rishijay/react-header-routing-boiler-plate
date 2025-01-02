29da2d914d274229077616389888dcd499e87242
Rishijay Shrivastava <rishijay.shrivastava@gmail.com>
2025-01-02 13:21:04 +0530

    Implemented redux toolkit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,14 @@
 // src/pages/Contact.js
 import React from "react";
+import { useSelector } from "react-redux";
 
 function Contact() {
+  const count = useSelector((state) => state.reducers);
   return (
     <div>
       <h2>Contact Page</h2>
       <p>Get in touch with us.</p>
+      <span>Latest count is {count}</span>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
 import logo from "../logo.svg";
 import "../App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { decrement, increment } from "../redux/reducer";
 
 function Home() {
+  const count = useSelector((state) => state.reducers);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log(count);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +26,18 @@ function Home() {
         >
           Learn React
         </a>
+        <div
+          style={{
+            margin: 10,
+            justifyContent: "space-evenly",
+            display: "flex",
+            width: "100%",
+          }}
+        >
+          <button onClick={() => dispatch(decrement())}>Decrement</button>
+          <span>Count: {count}</span>
+          <button onClick={() => dispatch(increment())}>Increment</button>
+        </div>
       </header>
     </div>
   );
